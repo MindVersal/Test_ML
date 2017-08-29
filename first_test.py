cbf149a93270a46e21c5cdcc51c6a9b67336fbe7
MindVersal <antajchik@gmail.com>
2017-08-29 10:14:24 +0300

    10:14 29.08.2017

--- first_test.py
+++ first_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def test_matplotlib():
@@ -9,5 +10,14 @@ def test_matplotlib():
     plt.show()
 
 
+def test_pandas():
+    data = {'Name': ['John', 'Anna', 'Peter', 'Linda'],
+            'Location': ['New York', 'Paris', 'Berlin', 'London'],
+            'Age': [24, 13, 53, 33]}
+    data_pandas = pd.DataFrame(data)
+    print('Data from Pandas:\n{}'.format(data_pandas[data_pandas.Age > 30]))
+
+
 if __name__ == '__main__':
-    test_matplotlib()
+    # test_matplotlib()
+    test_pandas()
