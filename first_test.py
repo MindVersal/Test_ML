58b7dafcc12702ddb60efd022cac6f1fc5878bcc
MindVersal <antajchik@gmail.com>
2017-08-29 11:58:48 +0300

    11:55 29.08.2017

--- first_test.py
+++ first_test.py
@@ -5,6 +5,7 @@ import mglearn
 from IPython.display import display
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def test_matplotlib():
@@ -39,7 +40,19 @@ def test_iris():
     iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
     pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
                       hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
-    plt.show(block=True)
+    # plt.show(block=True)
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+    X_new = np.array([[5, 2.9, 1, 0.2]])
+    print('Shape array X_new: {}'.format(X_new.shape))
+    prediction = knn.predict(X_new)
+    print('Predict: {}'.format(prediction))
+    print('Predicting mark: {}'.format(iris_dataset['target_names'][prediction]))
+    y_pred = knn.predict(X_test)
+    print('Predicts for test array:\n {}'.format(y_pred))
+    print('Correct on test array: {}'.format(np.mean(y_pred == y_test)))
+    print('Score on test data: {}'.format(knn.score(X_test, y_test)))
+
 
 if __name__ == '__main__':
     # test_matplotlib()
