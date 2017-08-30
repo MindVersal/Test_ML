c2ef4aa78e84d5274e5f60b323204b4ed3589471
MindVersal <antajchik@gmail.com>
2017-08-30 12:57:47 +0300

    13:00 30.08.2017

--- second_test.py
+++ second_test.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import mglearn
 from IPython.display import display
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_boston
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 
 def test_dataset_forge():
@@ -12,11 +16,20 @@ def test_dataset_forge():
     plt.xlabel('First feature.')
     plt.ylabel('Second feature.')
     print('Shape array: {}'.format(X.shape))
+    fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+    for n_neighbors, ax in zip([1, 3, 9], axes):
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+        mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=0.4)
+        mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+        ax.set_title('Count neighbors:{}'.format(n_neighbors))
+        ax.set_xlabel('Feature 0')
+        ax.set_ylabel('Feature 1')
+    axes[0].legend(loc=3)
     plt.show()
 
 
 def test_dataset_wave():
-    X, y = mglearn.datasets.make_wave(n_samples=40)
+    X, y = mglearn.datasetsw.make_wave(n_samples=40)
     plt.plot(X, y, 'o')
     plt.ylim(-3, 3)
     plt.xlabel('Feature')
@@ -24,6 +37,41 @@ def test_dataset_wave():
     plt.show()
 
 
+def test_dataset_cancer():
+    cancer = load_breast_cancer()
+    print('Keys in cancer: \n{}'.format(cancer.keys()))
+    print('Shape data array in cancer: \n{}'.format(cancer.data.shape))
+    print('Count primes for every classes in data cancer: \n{}'.format(
+        {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}
+    ))
+    print('Feature names: \n{}'.format(cancer.feature_names))
+    X_train, X_test, y_train, y_test = train_test_split(
+        cancer.data, cancer.target, stratify=cancer.target, random_state=66
+    )
+    training_accuracy = []
+    test_accuracy = []
+    neighbors_settings = range(1, 11)
+    for n_neighbors in neighbors_settings:
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+        clf.fit(X_train, y_train)
+        training_accuracy.append(clf.score(X_train, y_train))
+        test_accuracy.append(clf.score(X_test, y_test))
+    plt.plot(neighbors_settings, training_accuracy, label='Correct in training')
+    plt.plot(neighbors_settings, test_accuracy, label='Correct in testing')
+    plt.ylabel('Correct')
+    plt.xlabel('Count neighbors')
+    plt.legend()
+    plt.show()
+
+def test_dataset_boston():
+    boston = load_boston()
+    print('Keys in boston: \n{}'.format(boston.keys()))
+    print('Shape array boston: \n{}'.format(boston.data.shape))
+    X, y = mglearn.datasets.load_extended_boston()
+    print('Shape extended boston: \n{}'.format(X.shape))
+
 if __name__ == '__main__':
     # test_dataset_forge()
-    test_dataset_wave()
+    # test_dataset_wave()
+    test_dataset_cancer()
+    # test_dataset_boston()
