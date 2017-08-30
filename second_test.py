c360276c4fa12af5fd436e1d37ba2794c0c9cd6f
MindVersal <antajchik@gmail.com>
2017-08-30 15:56:34 +0300

    15:51 30.08.2017

--- second_test.py
+++ second_test.py
@@ -7,6 +7,8 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.datasets import load_boston
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 
 
@@ -30,11 +32,13 @@ def test_dataset_forge():
 
 
 def test_dataset_wave():
+    # first plot
     X, y = mglearn.datasets.make_wave(n_samples=40)
     plt.plot(X, y, 'o')
     plt.ylim(-3, 3)
     plt.xlabel('Feature')
     plt.ylabel('Base variable')
+    # second plot
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     reg = KNeighborsRegressor(n_neighbors=3)
     reg.fit(X_train, y_train)
@@ -54,6 +58,21 @@ def test_dataset_wave():
         ax.set_ylabel('Base variable')
     axes[0].legend(['Прогноз модели', 'Train data/answers', 'Test data/answers'], loc='best')
     plt.show()
+
+
+def test_dataset_wave_linear_regression_basic():
+    mglearn.plots.plot_linear_regression_wave()
+    plt.show()
+
+
+def test_dataset_wave_linear_regression_ols():
+    X, y = mglearn.datasets.make_wave(n_samples=60)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    lr = LinearRegression().fit(X_train, y_train)
+    print('lr.coef_: {}'.format(lr.coef_))
+    print('lr.intercept_: {}'.format(lr.intercept_))
+    print('Correct on train array: {:.2f}'.format(lr.score(X_train, y_train)))
+    print('Correct on test array: {:.2f}'.format(lr.score(X_test, y_test)))
 
 
 def test_dataset_cancer():
@@ -89,9 +108,36 @@ def test_dataset_boston():
     print('Shape array boston: \n{}'.format(boston.data.shape))
     X, y = mglearn.datasets.load_extended_boston()
     print('Shape extended boston: \n{}'.format(X.shape))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    lr = LinearRegression().fit(X_train, y_train)
+    print('Correct on train array: {:.2f}'.format(lr.score(X_train, y_train)))
+    print('Correct on test array: {:.2f}'.format(lr.score(X_test, y_test)))
+    ridge = Ridge().fit(X_train, y_train)
+    print('Ridge testing:')
+    print('Correct on train array: {:.2f}'.format(ridge.score(X_train, y_train)))
+    print('Correct on test array: {:.2f}'.format(ridge.score(X_test, y_test)))
+    ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+    print('Correct on train array (Ridge alpha=10): {:.2f}'.format(ridge10.score(X_train, y_train)))
+    print('Correct on test array (Ridge alpha=10): {:.2f}'.format(ridge10.score(X_test, y_test)))
+    ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+    print('Correct on train array (Ridge alpha=0.1): {:.2f}'.format(ridge01.score(X_train, y_train)))
+    print('Correct on test array (Ridge alpha=0.1): {:.2f}'.format(ridge01.score(X_test, y_test)))
+    plt.plot(ridge.coef_, 's', label='Ridge regression alpha=1')
+    plt.plot(ridge10.coef_, '^', label='Ridge regression alpha=10')
+    plt.plot(ridge01.coef_, 'v', label='Ridge regression alpha=0.1')
+    plt.plot(lr.coef_, 'o', label='Linear regression')
+    plt.xlabel('Index coef')
+    plt.ylabel('Score coef')
+    plt.hlines(0, 0, len(lr.coef_))
+    plt.ylim(-25, 25)
+    plt.legend()
+    plt.show()
+
 
 if __name__ == '__main__':
     # test_dataset_forge()
-    test_dataset_wave()
+    # test_dataset_wave()
     # test_dataset_cancer()
-    # test_dataset_boston()
+    test_dataset_boston()
+    # test_dataset_wave_linear_regression_basic()
+    # test_dataset_wave_linear_regression_ols()
